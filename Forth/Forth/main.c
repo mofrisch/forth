b4e252dd2ab8f7706b2ae90683c7b822c3820ccf
Moritz Frisch <mo68@posteo.net>
2019-11-19 16:43:36 +0100

    initial release of Xcode project

---
+++ Forth/Forth/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Forth
+//
+//  Created by Moritz Frisch on 19.11.19.
+//  Copyright © 2019 Moritz Frisch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
